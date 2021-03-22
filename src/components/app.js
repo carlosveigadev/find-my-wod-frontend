605fe52c8607bdcedb37dcaf07605572254d0b28
wrack <carlosveiga1618@gmail.com>
2021-03-22 13:40:15 -0300

    add main components and install axios

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard';
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
