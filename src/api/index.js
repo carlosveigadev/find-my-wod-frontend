184e808c6cc5d04cc0d23a9b27ca27b265251d1e
wrack <carlosveiga1618@gmail.com>
2021-03-22 14:30:47 -0300

    fix http error

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://localhost:3001';
+const URL = 'http://localhost:3001';
 
 const submitUserData = object => {
   const { email, password, passwordConfirmation } = object;
