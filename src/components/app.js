de2f0d677ef7138f476f743b650606db24849256
wrack <carlosveiga1618@gmail.com>
2021-03-23 12:30:57 -0300

    add request call to check if user is logged in in sessions api

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Routes from '../routes';
 
 const App = () => {
@@ -7,6 +8,24 @@ const App = () => {
   const updateInfo = data => {
     setUserInfo(data);
   };
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/logged_in', { withCredentials: true })
+      .then(response => {
+        console.log(response);
+        console.log(userInfo);
+        if (response.data.logged_in && userInfo.loggedInStatus === 'NOT_LOGGED_IN') {
+          updateInfo(
+            { loggedInStatus: 'LOGGED_IN', user: response.data.user },
+          );
+        } else if (!response.data.logged_in && userInfo.loggedInStatus === 'LOGGED_IN') {
+          updateInfo(
+            { loggedInStatus: 'NOT_LOGGED_IN', user: {} },
+          );
+        }
+      })
+      .catch(error => console.log(error));
+  }, []);
 
   return (
     <div className="app">
