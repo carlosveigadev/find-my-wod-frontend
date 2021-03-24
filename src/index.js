7d23d9d4f7f8d36dc47cf6447bf1b4132029382a
wrack <carlosveiga1618@gmail.com>
2021-03-24 18:09:16 -0300

    add store to provider and create store in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import userReducer from './redux/reducers/user';
 
-// add rootreducer combining stores
-// create store
+/* eslint-disable no-underscore-dangle */
+const store = createStore(userReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-enable */
 
 ReactDOM.render(
-  // <Provider>
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
-  // </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
