8e99994999dccde75a7795e654b1b07f211f908b
wrack <carlosveiga1618@gmail.com>
2021-03-24 16:48:37 -0300

    add react router dom dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+
+// add rootreducer combining stores
+// create store
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
