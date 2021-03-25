811ede978eaa7e57c86741bffe5fb4aa2ff4a306
wrack <carlosveiga1618@gmail.com>
2021-03-25 16:37:11 -0300

    remove not necessary console.logs

--- src/api-requests/index.js
+++ src/api-requests/index.js
@@ -2,16 +2,13 @@ import axios from 'axios';
 
 const URL = 'http://localhost:3001';
 
-const logInUser = data => {
-  console.log(data);
-  return axios({
-    url: `${URL}/auth/login`,
-    data: JSON.stringify(data),
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(res => console.log(res)).catch(err => console.log(err));
-};
+const logInUser = data => axios({
+  url: `${URL}/auth/login`,
+  data: JSON.stringify(data),
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then(res => console.log(res)).catch(err => err);
 
 export default logInUser;
