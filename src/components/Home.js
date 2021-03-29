f61ed9acb6c5dc08984ee465999c870c025217fb
wrack <carlosveiga1618@gmail.com>
2021-03-28 22:40:33 -0300

    add styles to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  Center, Flex, Box, Text,
+} from '@chakra-ui/react';
 import Navbar from './Navbar';
 import Wods from '../containers/Wods';
+import style from '../assets/css/Home.module.css';
 
 const Home = ({ isLoggedIn }) => {
   if (isLoggedIn) {
@@ -17,10 +21,26 @@ const Home = ({ isLoggedIn }) => {
     );
   }
   return (
-    <>
-      <Link to={{ pathname: '/login' }}>Log In</Link>
-      <Link to={{ pathname: '/signin' }}>Sign In</Link>
-    </>
+    <Box
+      bgSize="100% 100%"
+      bgPosition="Center"
+      bgImage="url(../assets/images/logo.jpg)"
+      bgRepeat="no-repeat"
+    >
+      <Box bgColor="rgb(255, 255, 255, 0.65)">
+        <Center color="black">
+          <Flex flexDirection="column" h="100vh" pt="35vh">
+            <Box py="1em">
+              <Text align="center" fontSize="xl">Welcome to Find my Wod!</Text>
+              <Text align="center" fontSize="xs">Choose an option to start.</Text>
+            </Box>
+            <Link class={style.button} to={{ pathname: '/login' }}> Log In </Link>
+            <Link class={style.button} to={{ pathname: '/signin' }}>Sign In</Link>
+          </Flex>
+        </Center>
+      </Box>
+
+    </Box>
   );
 };
 
