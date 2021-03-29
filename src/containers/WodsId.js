c8dfab61344aaa81b4f05f5c494373ee78f91b9f
wrack <carlosveiga1618@gmail.com>
2021-03-29 16:04:50 -0300

    add styles to WodsId container

--- src/containers/WodsId.js
+++ src/containers/WodsId.js
@@ -1,10 +1,13 @@
-import { Center, Flex } from '@chakra-ui/react';
+import {
+  AspectRatio, Center, Flex, Box, Heading, Text,
+} from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import { AiOutlineBackward } from 'react-icons/ai';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { favouriteRequest, fetchFavourites } from '../api-requests';
 import { favouriteData } from '../redux/actions';
+import style from '../assets/css/WodsId.module.css';
 
 const WodsId = ({
   location, favouriteWods, userToken, favouriteData,
@@ -22,28 +25,41 @@ const WodsId = ({
     favouriteData(newData);
   };
 
-  return (
-    <>
-      <Flex>
-        <Center>
-          <button
-            type="button"
-            onClick={() => {
-              history.goBack();
-            }}
-          >
-            <AiOutlineBackward fontSize="2em" color="darkgray" />
-          </button>
-        </Center>
-        <h1>{wod.title}</h1>
-        <h2>{wod.description}</h2>
-        <h2>{wod.image}</h2>
-        {ids.includes(wod.id)
-          ? <button type="button" id="unfavourite" onClick={handleSubmit}>Unstar</button>
-          : <button type="button" id="favourite" onClick={handleSubmit}>Star</button>}
+  const video = `https://www.youtube.com/embed/${wod.image}`;
 
-      </Flex>
-    </>
+  return (
+    <Flex direction="column" mt="2em" alignItems="center">
+      <Center>
+        <button
+          type="button"
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          <AiOutlineBackward fontSize="2em" color="darkgray" />
+        </button>
+      </Center>
+      <Box mt="2em">
+        <AspectRatio maxW="440px" ratio={4 / 3}>
+          <iframe
+            title={wod.title}
+            src={video}
+            allowFullScreen
+          />
+        </AspectRatio>
+        <Box mx="2em" mt="1em">
+          <Heading s="h1" size="lg">{wod.title}</Heading>
+          <Text my="1em">{wod.description}</Text>
+          {ids.includes(wod.id)
+            ? (
+              <button className={style.button} type="button" id="unfavourite" onClick={handleSubmit}>
+                Add to Favourites
+              </button>
+            )
+            : <button className={style.button} type="button" id="favourite" onClick={handleSubmit}>Remove from Favourites</button>}
+        </Box>
+      </Box>
+    </Flex>
   );
 };
 
