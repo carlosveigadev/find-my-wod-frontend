9ebeda21835eadcc18ac6b67a8960e3569ddcbc3
wrack <carlosveiga1618@gmail.com>
2021-03-30 18:01:10 -0300

    finish test configs

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,9 @@
-global.matchMedia = global.matchMedia || function () {
-  return {
-    matches: false,
-    addListener() {},
-    removeListener() {},
+global.matchMedia = global.matchMedia
+  // eslint-disable-next-line func-names
+  || function () {
+    return {
+      matches: false,
+      addListener() {},
+      removeListener() {},
+    };
   };
-};
